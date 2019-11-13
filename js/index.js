97f7aa337293e79cff24d3e72ae1616aa221990a
Alexandre Araujo Saura <alexandresaura21@gmail.com>
2019-11-13 12:34:12 -0300

    Alterando css e paginação

--- js/index.js
+++ js/index.js
@@ -1,8 +1,9 @@
 const quantidadePagina = 22;
 const quantidadeDeputados = 24;
 
+var paginaAtual = 1;
+
 $(document).ready(function() {
-    imprimePaginas();
     imprimeDeputados(1);
 });
 
@@ -31,18 +32,51 @@ function carregarDeputados(page) {
     return deputados;
 }
 
-function imprimePaginas() {
-    for(let i = 1; i <= quantidadePagina; i++){
+function imprimePaginas(page) {
+    let totalValores = quantidadePagina * quantidadeDeputados;
+    paginaAtual = page;
+
+    pageInicial = page - 2;
+    pageFinal = page + 2;
+
+    if(pageInicial < 1) {
+        pageFinal -= pageInicial - 1;
+        pageInicial = 1;
+    }
+
+    if(pageFinal > quantidadePagina) {
+        pageInicial -= (pageFinal - quantidadePagina);
+        pageFinal = quantidadePagina;
+    }
+
+    $('.pagination').html("");
+    $('.pagination').append(`
+        <li class="page-item ${paginaAtual - 1 < 1 ? 'disabled' : ''}">
+            <a onclick="imprimeDeputados(${paginaAtual - 1})" class="page-link text-dark" id="page-previous-tab" data-toggle="tab" href="#page${paginaAtual - 1}" role="tab" aria-label="Previous" aria-selected="false" ${paginaAtual - 1 < 1 ? 'aria-disabled="true"' : ''}>
+                <span aria-hidden="true">&laquo;</span>
+            </a>
+        </li>
+    `);
+    for(let i = pageInicial; i <= pageFinal; i++){
         $('.pagination').append(`
-            <li class="page-item">
+            <li class="page-item ${i == page ? 'active' : ''}">
                 <a onclick="imprimeDeputados(${i})" class="page-link text-dark" id="page${i}-tab" data-toggle="tab" href="#page${i}" role="tab" aria-controls="page${i}" aria-selected="false">${i}</a>
             </li>
         `);
     }
+    $('.pagination').append(`
+        <li class="page-item ${paginaAtual + 1 > quantidadePagina ? 'disabled' : ''}">
+            <a onclick="imprimeDeputados(${paginaAtual + 1})" class="page-link text-dark" id="page-next-tab" data-toggle="tab" href="#page${paginaAtual + 1}" role="tab" aria-label="Next" aria-selected="false" ${paginaAtual + 1 > quantidadePagina ? 'aria-disabled="true"' : ''}>
+                <span aria-hidden="true">&raquo;</span>
+            </a>
+        </li>
+    `);
 }
 
 function imprimeDeputados(page) {
     let deputados = new carregarDeputados(page);
+
+    imprimePaginas(page);
 
     $('#listaDeputados').html("");
     for(let indice in deputados){
